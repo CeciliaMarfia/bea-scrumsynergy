263afd245013fd81666da34a7ec85836d2046213
Lola Rodriguez <Rodriguezlola212@gmail.com>
2025-05-14 19:40:40 -0300

    Initial Django project setup

---
+++ tasks/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html');
+
+from django.contrib import messages
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
